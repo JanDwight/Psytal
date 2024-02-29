e73d5348d39f6a952c902447bfb887a04abdfe14
jjhonalyn <127106297+jjhonalyn@users.noreply.github.com>
2024-02-29 16:54:45 +0800

    Classes filter- not yet done

--- Thesis/psytal/vite/src/views/authorized_view/views_main/Classes.jsx
+++ Thesis/psytal/vite/src/views/authorized_view/views_main/Classes.jsx
@@ -1,6 +1,7 @@
 import {React, Fragment, useState} from "react";
 import { Menu, Transition } from '@headlessui/react';
 import ReactModal from 'react-modal';
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import AddClass from "../views_components/AddClass";
 import ClassPopUp from "../views_components/ClassPopUp";
 import ClassList from '../views_components/ClassList.jsx';
@@ -25,11 +26,23 @@ import ClassList from '../views_components/ClassList.jsx';
   );
 };
 
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
 export default function Classes(){
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isClassModalOpen, setIsClassModalOpen] = useState(false);
     const [filterText, setFilterText] = useState(''); // Filter text state
     const [activeTab] = useState(1); // Initialize active tab
+    const [selectedYear, setSelectedYear] = useState(null);
+    
+    //filter year_level
+    const handleYearSelect = (year) => {
+      setSelectedYear(year);
+      // Perform any other actions based on the selected year
+  };
+
 
     return(
     <>
@@ -66,7 +79,7 @@ export default function Classes(){
 
                 </div>
             </div>
-            <div className="table-container px-4 overflow-x-auto max-h-[400px] overflow-y-auto">
+            <div className="table-container px-4 overflow-x-auto max-h-[400px]">
         <table className="table w-full table-striped text-gray-700 mt-5">
             <thead >
               <tr >
@@ -74,7 +87,97 @@ export default function Classes(){
                         <th className="text-center bg-gray-200 p-2" style={{ width: "10%" }}>Course Code</th>
                         <th className="bg-gray-200 text-center p-2" style={{ width: "30%" }}>Course Title</th>
                         <th className="bg-gray-200 text-center p-2" style={{ width: "5%" }}>Semester</th>
-                        <th className="bg-gray-200 text-center p-2" style={{ width: "10%" }}>Year</th> 
+                        {/* <th className="bg-gray-200 text-center p-2" style={{ width: "10%" }}>Year</th>  */}
+                        <th className="bg-gray-200 text-center p-2 z-10" style={{ width: "10%" }}>
+                        <Menu as="div" className="relative block text-left z-10">
+                        <div>
+                          <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                            Year
+                            <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                          </Menu.Button>
+                        </div>
+
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="relative right-0 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                            <div className="py-1">
+                            <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    ALL
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    1st Year
+                                  </a>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    2nd Year
+                                  </a>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    3rd Year
+                                  </a>
+                                )}
+                              </Menu.Item>
+                              <Menu.Item>
+                                {({ active }) => (
+                                  <a
+                                    href="#"
+                                    className={classNames(
+                                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                      'block px-4 py-2 text-sm'
+                                    )}
+                                  >
+                                    4th Year
+                                  </a>
+                                )}
+                              </Menu.Item>
+                            </div>
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                      </th>
                         <th className="bg-gray-200 text-center p-2" style={{ width: "10%" }}>Section</th> 
                         <th className="bg-gray-200 text-centert p-2" style={{ width: "15%" }}>Instructor</th>
                         <th className="bg-gray-200 text-center p-2" style={{ width: "10%" }}>Action</th>
