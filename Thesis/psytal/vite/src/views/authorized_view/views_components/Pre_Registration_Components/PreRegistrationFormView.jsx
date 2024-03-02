6860a6dae5aebb70dd1d2e6909fd6489b691fdcf
rem <ganase.remcarr.00@gmail.com>
2024-03-02 17:37:06 +0800

    pending_dont_merge

--- Thesis/psytal/vite/src/views/authorized_view/views_components/Pre_Registration_Components/PreRegistrationFormView.jsx
+++ Thesis/psytal/vite/src/views/authorized_view/views_components/Pre_Registration_Components/PreRegistrationFormView.jsx
@@ -1696,6 +1696,34 @@ const handleChangeUnits = (index, value) => {
                             </div> 
                             {/*fix the two buttons above, no axios connection yet, do for other view*/}
                     </div>
+                    <div className="overflow-x-auto">
+                        <table className="min-w-full divide-y divide-gray-200">
+                          <thead>
+                              <tr>
+                                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 uppercase tracking-wider">Course Code</th>
+                                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 uppercase tracking-wider">Course Title </th>
+                                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 uppercase tracking-wider">Course Type</th>
+                                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 uppercase tracking-wider">Year</th>
+                                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 uppercase tracking-wider">Semester</th>
+                                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 uppercase tracking-wider">Units</th>
+                                  <th className="px-6 py-3 bg-gray-50 text-left text-xs leading-4 uppercase tracking-wider">Completion Status</th>
+                              </tr>
+                          </thead>
+                          <tbody className="bg-white divide-y divide-gray-200">
+                          {subjectData.map((item, index) => (
+                              <tr key={index}>
+                                  <td className="px-6 py-4 whitespace-no-wrap">{item.course_code}</td>
+                                  <td className="px-6 py-4 whitespace-no-wrap">{item.course_title}</td>
+                                  <td className="px-6 py-4 whitespace-no-wrap">{item.course_type}</td>
+                                  <td className="px-6 py-4 whitespace-no-wrap">{item.class_year}</td>
+                                  <td className="px-6 py-4 whitespace-no-wrap">{item.semester}</td>
+                                  <td className="px-6 py-4 whitespace-no-wrap">{item.units}</td>
+                                  <td className="px-6 py-4 whitespace-no-wrap">To Complete</td>
+                              </tr>
+                          ))}
+                          </tbody>
+                        </table>
+                    </div>
                 </div>
             </form>
         </div>
