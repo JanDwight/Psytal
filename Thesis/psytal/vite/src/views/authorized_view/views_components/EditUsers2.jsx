f4fd7133c5ef6c45bdc85fa6cb635d70f8cdb77a
rem <ganase.remcarr.00@gmail.com>
2024-03-24 11:18:51 +0800

    pahabol_2_update

--- Thesis/psytal/vite/src/views/authorized_view/views_components/EditUsers2.jsx
+++ Thesis/psytal/vite/src/views/authorized_view/views_components/EditUsers2.jsx
@@ -105,7 +105,7 @@ export default function EditUsers({ showModal, onClose, user }) {
                 >
                   <option value="1">Admin</option>
                   <option value="2">Staff</option>
-                  <option value="3">Instructor</option>
+                  <option hidden={true} value="3">Instructor</option>
                   <option value="4">Student</option>
                 </select>
               </div>
