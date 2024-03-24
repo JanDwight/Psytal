1a09fcb7bd9ba34e28fd2e66dd867e954e2c35d8
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-03-24 21:49:32 +0800

    Email Domains done

--- Thesis/psytal/vite/src/views/authorized_view/views_components/Settings/EditEmailDomain.jsx
+++ Thesis/psytal/vite/src/views/authorized_view/views_components/Settings/EditEmailDomain.jsx
@@ -1,72 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import axiosClient from '../../../../axios';
+import Feedback from '../../../feedback/Feedback';
 
 export default function EditEmailDomain({ closeModal, selectedEmailDomain }) {
     const [error, setError] = useState({__html: ""});
-    const [successMessage, setSuccessMessage] = useState(null);
-    const [emailDomain, setEmailDomain] = useState(
-        selectedEmailDomain && selectedEmailDomain.email_domains ? selectedEmailDomain.email_domains : ''
-    );
-    const emailDomainId = selectedEmailDomain && selectedEmailDomain.id ? selectedEmailDomain.id : ''
-    const [existingEmailDomains, setExistingEmailDomains] = useState('');
+    const [emailDomain, setEmailDomain] = useState(selectedEmailDomain.email_domains);
+    const [successMessage, setSuccessMessage] = useState('');
+    const [successStatus, setSuccessStatus] = useState('');
 
-    // Fetch all existing email domains from your data source
-    useEffect(() => {
-        axiosClient.get(`/getallemaildomains`)
-          .then(({ data }) => {
-            setExistingEmailDomains(data);          
-          })
-          .catch((error) => {
-            console.error('Error fetching existing email domains:', error);
-          });
-      }, []);
-
+    console.log('test',emailDomain);
     //update Email Domains
-    const onSubmit = (ev) => {
+    const onSubmit = async (ev) => {
         ev.preventDefault();
-        setError({ __html: "" });
 
-
-         // Check if the updated email domain already exists
-        const isDuplicate = existingEmailDomains.some(domain => domain.email_domains === emailDomain);
-
-        if (isDuplicate) {
-            setSuccessMessage({ message: "Email Domain Already Exists." });
-            setTimeout(() => {
-                setSuccessMessage(null); 
-              }, 1500);
-            return; // Exit the function 
-        }
-
-        axiosClient
-        .put(`/updateemaildomain/${emailDomainId}`, {
+        try {
+          const response = await axiosClient.put(`/updateemaildomain/${selectedEmailDomain.id}`, {
             email_domains: (emailDomain)
-        })
-        .then(({ data }) => {
-            // Handle success, e.g., show a success message
-            console.log(data);
-      
-            setSuccessMessage({
-              message: 'Email Domain Edited successfully!',
-            });
-      
-            setTimeout(() => {
-              setSuccessMessage(null);
-              window.location.reload(false);
-            }, 2000);
-          })
-          .catch((error) => {
-            // Handle errors, including validation errors
-            console.error('Error sending data:', error);
-            
-          });
+        });
+          setSuccessMessage(response.data.message);
+          setSuccessStatus(response.data.success);
+
+          // setTimeout(() => {
+          //   setSuccessMessage(null);
+          //   closeModal();
+          //   window.location.reload();
+          // }, 2000);
+        } catch (error) {
+            setSuccessMessage(error.response.data.message)
+            setSuccessStatus(error.response.data.success)
+        }
     }
 
     return (
         <>
-        <div className='text-center mb-4'>
-            <h2 className='text-2xl font-semibold text-[#65a30d]'>Edit Domain Modal</h2>
-        </div>
+        <Feedback isOpen={successMessage !== ''} onClose={() => setSuccessMessage('')} successMessage={successMessage} status={successStatus} refresh={false}/>  
+          <div className='text-center mb-4'>
+              <h2 className='text-2xl font-semibold text-[#65a30d]'>Edit Domain Modal</h2>
+          </div>
 
             <div className='flex justify-center'>
                 <form onSubmit={onSubmit}>
@@ -76,7 +46,6 @@ export default function EditEmailDomain({ closeModal, selectedEmailDomain }) {
                     <input 
                         id="emailDomain"
                         type="text" 
-                        placeholder={emailDomain}
                         value={emailDomain}
                         onChange={ev => setEmailDomain(ev.target.value)}
                     />
@@ -96,17 +65,6 @@ export default function EditEmailDomain({ closeModal, selectedEmailDomain }) {
                             </button>
                         </div> 
                 </form>
-                {successMessage && (
-                <div className="fixed top-0 left-0 w-full h-full overflow-y-auto bg-black bg-opacity-50">
-                <div className="lg:w-1/2 px-4 py-1 shadow-lg w-[20%] h-fit bg-[#FFFFFF] rounded-xl mt-[10%] mx-auto p-5">
-                    <div className="w-full px-4 mx-auto mt-6">
-                    <div className="text-center text-xl text-green-600 font-semibold my-3">
-                        {successMessage.message}
-                    </div>
-                    </div>
-                </div>
-                </div>
-            )}
             </div> 
         </>
     );
