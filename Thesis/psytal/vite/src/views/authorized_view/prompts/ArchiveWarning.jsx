c9880657758b3813cdfa23ee9f11e8de76b2bdf1
rem <ganase.remcarr.00@gmail.com>
2024-03-26 09:32:25 +0800

    user_friendly_prompts

--- Thesis/psytal/vite/src/views/authorized_view/prompts/ArchiveWarning.jsx
+++ Thesis/psytal/vite/src/views/authorized_view/prompts/ArchiveWarning.jsx
@@ -13,17 +13,17 @@ export default function ArchiveWarning({ closeModal, handleSave}) {
             <div className="mb-6 text-center"> 
             <strong>Warning</strong>
                 <p>
-                  Proceeding with this action will hide the selected item from view. 
-                  It cannot be modified unless restored from the <b>Dashboard</b> by an admin user.
+                  Proceeding with this action will remove the selected item from view. 
+                  It cannot be modified unless restored by the administrator from the <b>Dashboard</b>.
                   Are you sure you want to continue?
                 </p>
             </div>
             <div id="decision" className='text-center space-x-3'>
                 <button onClick={handleYes} className="bg-[#397439] hover:bg-[#0FE810] rounded-2xl  px-7 py-2 text-white font-size">
-                    Proceed
+                    Yes
                 </button>
                 <button onClick={closeModal} className="bg-red-600 hover:bg-red-700 rounded-2xl  px-7 py-2 text-white font-size">
-                    Cancel
+                    No
                 </button>
             </div>
         
